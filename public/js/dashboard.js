5309d241b7df9f2658aa58090072ead9e9689180
4FunkE <4FunkEcodes@gmail.com>
2023-09-07 13:27:26 -0400

    fixed paths in dashboard

---
+++ public/js/dashboard.js
@@ -0,0 +1,47 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const name = document.querySelector('#blogPost-name').value.trim();
+    const description = document.querySelector('#blogPost-desc').value.trim();
+  
+    if (name && needed_funding && description) {
+      const response = await fetch(`/api/blogPost`, {
+        method: 'POST',
+        body: JSON.stringify({ name, description }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to create Blogpost');
+      }
+    }
+  };
+  
+  const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/blogPosts/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to delete Blogpost');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.new-blogPost-form')
+    .addEventListener('submit', newFormHandler);
+  
+  document
+    .querySelector('.blogPost-list')
+    .addEventListener('click', delButtonHandler);
+  
