e227624d37f8063a434cb3056991553b4253a907
4FunkE <4FunkEcodes@gmail.com>
2023-09-07 16:27:43 -0400

    debugging, organizing

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,25 +1,26 @@
+// Import Dependencies and Models
 const sequelize = require('../config/connection');
 const { User, Blogpost } = require('../models');
-
+// Import Sample Data
 const userData = require('./userData.json');
 const blogPostData = require('./blogPostData.json');
 
 const seedDatabase = async () => {
-  await sequelize.sync({ force: true });
-
+  await sequelize.sync({ force: true }); //drops and recreates the tables in the database, effectively resetting it
+  // user creation
   const users = await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
-
+  // blog post creation
   for (const blogPost of blogPostData) {
     await Blogpost.create({
       ...blogPost,
       user_id: users[Math.floor(Math.random() * users.length)].id,
     });
   }
-
+// exit script
   process.exit(0);
 };
-
+// call the function to start database seeding process
 seedDatabase();
